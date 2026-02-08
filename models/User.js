cabdcbb4490c5ca3c64438f5f3411fadc0e1b43d
amitbossff <amitkr706186@gmail.com>
2026-02-08 18:46:18 +0530

    Create User.js

---
+++ models/User.js
@@ -0,0 +1,17 @@
+const mongoose = require("mongoose");
+
+const UserSchema = new mongoose.Schema({
+  username: { type: String, unique: true, required: true },
+  number: { type: String, unique: true, required: true },
+  password: { type: String, required: true },
+  telegramUid: { type: String, required: true },
+
+  balance: { type: Number, default: 0 },
+
+  isVerified: { type: Boolean, default: false },
+  isBlocked: { type: Boolean, default: false },
+
+  createdAt: { type: Date, default: Date.now }
+});
+
+module.exports = mongoose.model("User", UserSchema);
