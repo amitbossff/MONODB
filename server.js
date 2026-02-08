9fc7b92f664fc97a1b043237443d581dac189d53
amitbossff <amitkr706186@gmail.com>
2026-02-08 18:35:04 +0530

    Create server.js

---
+++ server.js
@@ -0,0 +1,21 @@
+require("dotenv").config();
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+mongoose.connect(process.env.MONGO_URI)
+  .then(() => console.log("MongoDB Connected"))
+  .catch(err => console.error(err));
+
+app.get("/", (req, res) => {
+  res.send("Lifafa Backend Running 🚀");
+});
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log("Server started on port " + PORT);
+});
