aa40524abdeb3b41af5fa574c66c304d6935c378
amitbossff <amitkr706186@gmail.com>
2026-02-08 19:15:46 +0530

    Create Code.js

---
+++ models/Code.js
@@ -0,0 +1,11 @@
+const mongoose = require("mongoose");
+
+const CodeSchema = new mongoose.Schema({
+  user: { type: mongoose.Schema.Types.ObjectId, ref: "User" },
+  code: { type: String, unique: true },
+  numbers: [String],
+  used: { type: Boolean, default: false },
+  createdAt: { type: Date, default: Date.now }
+});
+
+module.exports = mongoose.model("Code", CodeSchema);
